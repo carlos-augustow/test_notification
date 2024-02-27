7af578f96cf81d945990f7923eb1113c7f079a78
carlos-augustow <85113496+carlos-augustow@users.noreply.github.com>
2024-02-27 10:56:46 -0300

    asdsad

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import {ToastContainer, Zoom} from "react-toastify";
+import logo from "./logo.svg";
+import "./App.css";
+import { ToastContainer, Zoom } from "react-toastify";
 import Notification from "./firebaseNotifications/Notification";
 
 function App() {
   return (
     <div className="App">
       <ToastContainer
-          position="bottom-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={true}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          transition={Zoom}
-          closeButton={false}
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={true}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        transition={Zoom}
+        closeButton={false}
       />
-        <Notification/>
+      <Notification />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloadAAAwwAA.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
